e9c6bde20d1145f7ae5e7ae0e960df62d9bf3658
aoi <ruriirono-hanaba-6ttun-inti@docomo.ne.jp>
2020-07-04 15:42:27 +0000

    課題提出11

---
+++ php001.php
@@ -0,0 +1,36 @@
+<?php
+$a = 3;
+$b = 7;
+$newa = $a + $b;
+echo $newa;
+
+$array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+echo $array_month[7];
+
+$hello = "Hello,";
+$name = "Yoshizawa";
+$world = "'s World!";
+echo $hello;
+echo $name;
+echo $world;
+
+$tech_boost = "tech";
+$tech_boost .= ' boost';
+echo $tech_boost;
+
+$calender_2018 = [
+"January" => "1月",
+"Febuary" => "2月",
+"March" => "3月",
+"April" => "4月",
+"May" => "5月",
+"June" => "6月",
+"July" => "7月",
+"August" => "8月",
+"September" => "9月",
+"October" => "10月",
+"November" => "11月",
+"December" => "12月",
+];
+
+echo $calender_2018["December"];
